55294adaef8c4a8ac64b97bea930f6db442c4998
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-04-11 15:26:44 +0300

    XML
    deserializing and serializing data from and to XML

---
+++ 09.MXL Processing/ProductShop/ProductShop/Utilities/XmlHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string xml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(xml);
+
+            T deserializedDTOs = (T)serializer.Deserialize(reader)!;
+
+            return deserializedDTOs;
+        }
+
+        public string Serialize<T>(T obj, string rootName)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+
+            serializer.Serialize(writer, obj, namespaces);
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
