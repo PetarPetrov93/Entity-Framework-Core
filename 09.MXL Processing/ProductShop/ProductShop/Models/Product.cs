039d3e910889a67c2c8a1e1db2f99cfe416a3dbf
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-04-11 15:26:17 +0300

    XML
    deserializing and serializing data from and to XML

---
+++ 09.MXL Processing/ProductShop/ProductShop/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Product
+    {
+        public Product()
+        {
+            this.CategoryProducts = new List<CategoryProduct>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int SellerId { get; set; }
+
+        [InverseProperty(nameof(User.ProductsSold))]
+        public virtual User Seller { get; set; } = null!;
+
+        public int? BuyerId { get; set; }
+        [InverseProperty(nameof(User.ProductsBought))]
+        public virtual User? Buyer { get; set; }
+
+        public virtual ICollection<CategoryProduct> CategoryProducts { get; set; }
+    }
+}
