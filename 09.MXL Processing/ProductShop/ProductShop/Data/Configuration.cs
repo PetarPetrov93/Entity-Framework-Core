cba657cfc27e96ae2952a0ae747b73e052e734e6
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-04-11 15:30:32 +0300

    XML
    deserializing and serializing data from and to XML

--- 09.MXL Processing/ProductShop/ProductShop/Data/Configuration.cs
+++ 09.MXL Processing/ProductShop/ProductShop/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public const string ConnectionString = @"Server=PETAR;Database=ProductShop;Integrated Security=True;Encrypt=False";
+        public const string ConnectionString = @"Server=.;Database=ProductShop;Integrated Security=True;Encrypt=False";
     }
 }
