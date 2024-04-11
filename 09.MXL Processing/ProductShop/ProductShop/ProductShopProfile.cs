f7eb2eca971be37bc5cacaf977f3c6117e2c5a9d
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-04-11 15:27:05 +0300

    XML
    deserializing and serializing data from and to XML

---
+++ 09.MXL Processing/ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Newtonsoft.Json.Serialization;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            CreateMap<ImportUserDTO, User>();
+
+            //Product
+            CreateMap<ImportProductDTO, Product>();
+
+            CreateMap<Product, ExportProductsInRangeDTO>()
+                .ForMember(dest => dest.Buyer, opt => opt.MapFrom(p => p.Buyer.FirstName + " " + p.Buyer.LastName));
+
+            CreateMap<Product, ExportProductForUsersWithSoldItemsDTO>();
+
+            //Category
+            CreateMap<ImportCategoryDTO, Category>();
+
+            //CategoryProduct
+            CreateMap<ImportCategoryProductDTO, CategoryProduct>();
+        }
+    }
+}
