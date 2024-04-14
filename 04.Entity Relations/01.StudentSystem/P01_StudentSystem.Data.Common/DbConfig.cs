1de237456e8bc7f2209acda3d8b5d192ae8a96c6
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-04-14 16:23:28 +0300

    solution

---
+++ 04.Entity Relations/01.StudentSystem/P01_StudentSystem.Data.Common/DbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_StudentSystem.Data.Common
+{
+    public class DbConfig
+    {
+        public const string ConnectionString = @"Server=.;Database=Students;Integrated Security=True;Encrypt=False";
+    }
+}
---
+++ 04.Entity Relations/01.StudentSystem/P01_StudentSystem.Data.Common/PropLengthValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Common
+{
+    public class PropLengthValues
+    {
+        //Student
+        public const int NameLength = 100;
+
+        //Course
+        public const int CourseNameLength = 80;
+
+        //Resource
+        public const int ResourceNameLength = 50;
+    }
+}
