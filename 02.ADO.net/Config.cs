2ae668c0240b39d6104ccb9bc5afc0bc22eb4fe3
PetarPetrov93 <petarpetrov_93@abv.bg>
2024-04-17 09:16:13 +0300

    solution

---
+++ 02.ADO.net/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.VillainName
+{
+    internal static class Config
+    {
+        public const string ConncectionString = @"Server=.;Database=MinionsDB;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
